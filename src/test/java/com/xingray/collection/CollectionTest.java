7ed9c773ad8562686cbbc678cc5a1034303c46b0
leixing <leixing1012@qq.com>
2020-11-08 16:27:18 +0800

    增加获取最大值最小值的方法

--- src/test/java/com/xingray/collection/CollectionTest.java
+++ src/test/java/com/xingray/collection/CollectionTest.java
@@ -1,9 +1,11 @@
 package com.xingray.collection;
 
-import com.xingray.javabase.result.Result;
+import com.xingray.javabase.interfaces.IntMapper;
+import com.xingray.javabase.range.IntRange;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CollectionTest {
 
@@ -19,4 +21,63 @@ public class CollectionTest {
     }
 
 
+    @Test
+    public void test03() {
+        int[] values = {10, 3, 63, 23, 53, 62, 85, 87, 76, 45, 65};
+        IntRange intRange = CollectionUtil.intMinMax(values);
+        System.out.println(intRange);
+        assert intRange.getStart() == 3;
+        assert intRange.getEnd() == 87;
+    }
+
+    static class People {
+        public String name;
+        public String fullName;
+
+        public People(String name, String fullName) {
+            this.name = name;
+            this.fullName = fullName;
+        }
+    }
+
+    @Test
+    public void test04() {
+        List<People> peoples = List.of(new People("alex12", "ABC alex 222"),
+                new People("blue234", "ASD blue 4343"),
+                new People("coco1", "RFV coco 5455453")
+        );
+
+        IntRange nameLengthRange = CollectionUtil.intMinMax(peoples,
+                new IntMapper<People>() {
+                    @Override
+                    public int map(People people) {
+                        return people.name.length();
+                    }
+                }, new IntMapper<People>() {
+                    @Override
+                    public int map(People people) {
+                        return people.name.length();
+                    }
+                });
+        System.out.println(nameLengthRange);
+        assert nameLengthRange.getStart() == 5;
+        assert nameLengthRange.getEnd() == 7;
+
+        IntRange fullNameLengthRange = CollectionUtil.intMinMax(peoples,
+                new IntMapper<People>() {
+                    @Override
+                    public int map(People people) {
+                        return people.fullName.length();
+                    }
+                }, new IntMapper<People>() {
+                    @Override
+                    public int map(People people) {
+                        return people.fullName.length();
+                    }
+                });
+
+        System.out.println(fullNameLengthRange);
+        assert fullNameLengthRange.getStart() == 12;
+        assert fullNameLengthRange.getEnd() == 16;
+    }
 }
